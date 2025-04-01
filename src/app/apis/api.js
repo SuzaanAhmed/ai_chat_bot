9c8d51838222b68099fc7d4f0645b12b20049ca2
SuzaanAhmed <SuzaanAhmed00@gmail.com>
2025-04-01 13:56:34 +0530

    Beginning

---
+++ src/app/apis/api.js
@@ -0,0 +1,3 @@
+const API_KEY="AIzaSyASSYjwWyf6-y4bgmwhfDgKReUMawjdRo8"
+const BASE_URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+const API=`${BASE_URL}?key=${API_KEY}`
